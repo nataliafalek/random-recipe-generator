a29d338b8db3beacd160096cfab3c189cef94063
nataliafalek <faleknatalia@gmail.com>
2018-02-14 12:15:26 +0100

    poprawki cz. 2

--- server/src/main/java/com/faleknatalia/randomrecipegenerator/RandomRecipeGeneratorApplication.java
+++ server/src/main/java/com/faleknatalia/randomrecipegenerator/RandomRecipeGeneratorApplication.java
@@ -13,15 +13,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 import javax.transaction.Transactional;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableConfigurationProperties
 public class RandomRecipeGeneratorApplication implements CommandLineRunner {
+
+    private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(RandomRecipeGeneratorApplication.class);
 
     @Autowired
     RecipeRepository recipeRepository;
@@ -36,18 +40,17 @@ public class RandomRecipeGeneratorApplication implements CommandLineRunner {
     @Transactional
     public void run(String... strings) throws Exception {
 
-        Map<Long, Recipe> recipesMap;
-        List<Ingredient> listOfIngredient;
-
-        loadCusine("./server/src/main/resources/static/Cuisine");
-        listOfIngredient = readIngredientFromCsv("./server/src/main/resources/static/Ingredient_Data.csv");
-        recipesMap = readRecipeFromCsv("./server/src/main/resources/static/Recipe_Data.csv");
+        Map<Long, Recipe> recipesMap = readRecipeFromCsv("/static/Recipe_Data.csv");
+        List<Ingredient> listOfIngredient = readIngredientFromCsv("/static/Ingredient_Data.csv");
 
         changeIngredients(recipesMap, listOfIngredient, 1, 0, 5);
         changeIngredients(recipesMap, listOfIngredient, 2, 5, 11);
         changeIngredients(recipesMap, listOfIngredient, 3, 11, 16);
         changeIngredients(recipesMap, listOfIngredient, 4, 16, 25);
         changeIngredients(recipesMap, listOfIngredient, 5, 25, 34);
+
+        cuisineRepository.save(readCuisineFromFile("/static/Cuisine"));
+        recipeRepository.save(recipesMap.values());
 
     }
 
@@ -66,42 +69,41 @@ public class RandomRecipeGeneratorApplication implements CommandLineRunner {
 
         Map<Long, Recipe> recipesMap = new HashMap<>();
 
-        try (Stream<String> stream = Files.lines(Paths.get(path))) {
-            long i = 1;
-            for (String s : (Iterable<String>) () -> stream.skip(1).iterator()) {
-                String[] str = s.split(";");
-                recipesMap.put(i, new Recipe(str[1], str[2], Collections.emptyList(), str[3]));
-                i++;
-            }
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
+        try (Stream<String> stream = Files.lines(Paths.get(this.getClass().getResource(path).toURI()))) {
+            stream.skip(1).forEach(line -> {
+                String[] str = line.split(";");
+                recipesMap.put(Long.parseLong(str[0]), new Recipe(str[1], str[2], Collections.emptyList(), str[3]));
+
+            });
+        } catch (IOException|URISyntaxException e) {
+            logger.error("Can't read recipe from file", e);
         }
         return recipesMap;
     }
 
     private List<Ingredient> readIngredientFromCsv(String path) {
-        List<Ingredient> listOfIngredients = new ArrayList<>();
-        try (Stream<String> stream = Files.lines(Paths.get(path))) {
-            stream.skip(1).forEach(line -> {
+        try (Stream<String> stream = Files.lines(Paths.get(this.getClass().getResource(path).toURI()))) {
+            return stream.skip(1).map(line -> {
                 String[] str = line.split(";");
-                listOfIngredients.add(new Ingredient(str[1], str[3], Double.parseDouble(str[2])));
-            });
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
+                return (new Ingredient(str[1], str[3], Double.parseDouble(str[2])));
+            }).collect(Collectors.toList());
+        } catch (IOException|URISyntaxException e) {
+            logger.error("Can't read ingredients from file", e);
+            return new ArrayList<>();
         }
-        return listOfIngredients;
     }
 
     private void changeIngredients(Map<Long, Recipe> recipesMap, List<Ingredient> listOfIngredients, long recipeIdx, int ingredientIdxStart, int ingredientIdxEnd) {
         recipesMap.get(recipeIdx).setIngredients(listOfIngredients.subList(ingredientIdxStart, ingredientIdxEnd));
-        recipeRepository.save(recipesMap.get(recipeIdx));
     }
 
-    private void loadCusine(String path) {
-        try (Stream<String> stream = Files.lines(Paths.get(path))) {
-            stream.forEach(a -> cuisineRepository.save(new Cuisine(a)));
-        } catch (IOException e) {
-            e.printStackTrace();
+    private List<Cuisine> readCuisineFromFile(String path) {
+        List<Cuisine> cuisines = new ArrayList<>();
+        try (Stream<String> stream = Files.lines(Paths.get(this.getClass().getResource(path).toURI()))) {
+           cuisines = stream.map(a -> new Cuisine(a)).collect(Collectors.toList());
+        } catch (IOException|URISyntaxException e) {
+            logger.error("Can't load cuisines from file", e);
         }
+        return cuisines;
     }
 }
